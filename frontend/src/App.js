7fc8455564aefaa47abecaa119d8d241926089d2
AntonyStephen28 <antonystephen028@gmail.com>
2024-03-22 12:34:19 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter as  Route, Router,Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+   <>
+   {/* <Router>
+      <Navbar/>
+     
+   </Router> */}
+
+   </>
+  );
+}
+
+export default App;
